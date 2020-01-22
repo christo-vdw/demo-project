fd94a7be55036fc34e9c6f692dffa13498a7ebe0
christo <christo.vanderwesthuizen@vastech.co.za>
2020-01-22 22:26:44 +0200

    Adding data loader

---
+++ backend/src/main/java/com/example/demo/MockDataLoader.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.opencsv.CSVReader;
+
+@Component
+public class MockDataLoader {
+
+	private final String uri = "data/mock_match_results.csv";
+	
+	@Autowired
+	private MatchResultRepository matchResultRepository;
+
+	@PostConstruct
+	public void load() throws IOException, URISyntaxException  {
+		Reader reader = Files.newBufferedReader(Paths.get(ClassLoader.getSystemResource(uri).toURI()));
+		readAll(reader,(String[] data) -> matchResultRepository.save(new MatchResult(data[0], Integer.parseInt(data[1]), data[2], Integer.parseInt(data[3]))));
+	}
+
+	private List<String[]> readAll(Reader reader, Consumer<String[]> consumer) throws IOException  {
+		CSVReader csvReader = new CSVReader(reader);
+		List<String[]> list = new ArrayList<>();
+		csvReader.iterator().forEachRemaining(l -> consumer.accept(l));
+		reader.close();
+		csvReader.close();
+		return list;
+	}
+}
