72609be4b06b7f2ad08ef674f60c71a1c4b6b8c5
christo <christo.vanderwesthuizen@vastech.co.za>
2020-01-24 00:35:39 +0200

    Fixing Swagger config

--- backend/src/main/java/com/example/demo/SwaggerConfig.java
+++ backend/src/main/java/com/example/demo/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.example.demo"))
+				.apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
 				.paths(PathSelectors.regex("/.*")).build().apiInfo(apiEndPointsInfo());
 	}
 
