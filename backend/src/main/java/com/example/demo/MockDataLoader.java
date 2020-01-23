dc55b2e49a4f8e5954a691a540135150933e2915
christo <christo.vanderwesthuizen@vastech.co.za>
2020-01-23 23:28:37 +0200

    Fixing resource loading for compiled jar

--- backend/src/main/java/com/example/demo/MockDataLoader.java
+++ backend/src/main/java/com/example/demo/MockDataLoader.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
@@ -12,6 +11,8 @@ import java.util.function.Consumer;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 import com.opencsv.CSVReader;
@@ -19,15 +20,18 @@ import com.opencsv.CSVReader;
 @Component
 public class MockDataLoader {
 
-	private final String uri = "data/mock_match_results.csv";
+	private final String uri = "classpath:data/mock_match_results.csv";
+	
+	@Autowired
+	ResourceLoader resourceLoader;
 	
 	@Autowired
 	private MatchResultRepository matchResultRepository;
 
 	@PostConstruct
 	public void load() throws IOException, URISyntaxException  {
-		Reader reader = Files.newBufferedReader(Paths.get(ClassLoader.getSystemResource(uri).toURI()));
-		readAll(reader,(String[] data) -> matchResultRepository.save(new MatchResult(data[0].toUpperCase(), Integer.parseInt(data[1]), data[2].toUpperCase(), Integer.parseInt(data[3]))));
+		Resource resource = resourceLoader.getResource(uri);
+		readAll( new InputStreamReader(resource.getInputStream()),(String[] data) -> matchResultRepository.save(new MatchResult(data[0].toUpperCase(), Integer.parseInt(data[1]), data[2].toUpperCase(), Integer.parseInt(data[3]))));
 	}
 
 	private List<String[]> readAll(Reader reader, Consumer<String[]> consumer) throws IOException  {
